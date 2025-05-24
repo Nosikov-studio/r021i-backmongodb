982b3ea24b30fbc0925e3df1506d464f67006e61
Nos <zvezdazanatih@gmail.com>
2025-05-24 11:36:51 +0700

    20

--- __app.js
+++ __app.js
@@ -87,7 +87,28 @@ app.get('/api/:_id', async (req, res) => {
   }
 });
    
- 
+ //************** */ получаем один документ по API (json) по id (не у всех есть!)*****************
+
+
+app.get('/api/id/:id', async (req, res) => {
+  try {
+    const id = Number(req.params.id);
+
+    if (!ObjectId.isValid(id)) {
+      return res.status(400).json({ message: "Неверный формат id" });
+    }
+
+    const u = await collection.findOne({ id: id });
+
+    if (!u) {
+      return res.status(404).json({ message: "Документ не найден" });
+    }
+
+    res.status(200).json(u);
+  } catch (err) {
+    res.status(500).json({ message: "Ошибка при получении данных", error: err.message });
+  }
+});
 
 //************** */ Вставляем документ по API *****************
         app.post('/api', urlencodedParser, async (req, res) => {
@@ -110,12 +131,12 @@ app.get('/api/:_id', async (req, res) => {
 app.post('/api/edit', urlencodedParser, async (req, res) => {
       try {
          
-         const id = Number(req.body._id);
+         const id = req.body.id;
          const name = req.body.name;
          const age = req.body.age;
          const newUser = { id, name, age };
         // Получаем все документы из коллекции
-        result = await collection.findOneAndUpdate({_id: id}, { $set: {name: name, age: age}}, { returnDocument: "after" });;
+        result = await collection.findOneAndUpdate({id: id}, { $set: {name: name, age: age}}, { returnDocument: "after" });;
         // res.status(200).json(us);
         res.json(newUser);
 
@@ -124,11 +145,11 @@ app.post('/api/edit', urlencodedParser, async (req, res) => {
       }
     });
 //************** */ Удаляем документ по API *****************
-        app.post('/delete/:_id',urlencodedParser, async (req, res) => {
+        app.post('/delete/:id',urlencodedParser, async (req, res) => {
       try {
-          const id = Number(req.params._id);
+          const id = Number(req.params.id);
         // Получаем все документы из коллекции
-        result = await collection.deleteOne({_id: id});
+        result = await collection.deleteOne({id: id});
         // res.status(200).json(us);
         res.json(result);
 
